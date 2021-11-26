0484bd112c221dabdf10e686023a7c5ed8ed8b31
Carolyn777777712 <Carolyn777777712@gmail.com>
2021-11-26 20:52:49 +0800

    Refactor Brick

--- src/main/java/com/hfych2/brickdestroy/Brick.java
+++ src/main/java/com/hfych2/brickdestroy/Brick.java
@@ -1,12 +1,10 @@
 package com.hfych2.brickdestroy;
 
+
 import java.awt.*;
 import java.awt.Point;
-import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
-import java.lang.reflect.GenericDeclaration;
-import java.lang.reflect.TypeVariable;
-import java.util.Random;
+
 
 /**
  * Created by filippo on 04/09/16.
@@ -14,7 +12,6 @@ import java.util.Random;
  */
 abstract public class Brick  {
 
-    public static final int MIN_CRACK = 1;
     public static final int DEF_CRACK_DEPTH = 1;
     public static final int DEF_STEPS = 35;
 
@@ -49,24 +46,18 @@ abstract public class Brick  {
 
     protected abstract Shape makeBrickFace(Point pos,Dimension size);
 
+    public abstract Shape getBrick();
+
+    public final boolean isBroken(){
+        return broken;
+    }
+
     public  boolean setImpact(Point2D point , int dir){
         if(broken)
             return false;
         impact();
         return  broken;
     }
-
-    public abstract Shape getBrick();
-
-
-    public Color getBorderColor(){
-        return  border;
-    }
-
-    public Color getInnerColor(){
-        return inner;
-    }
-
 
     public final int findImpact(Ball b){
         if(broken)
@@ -83,21 +74,23 @@ abstract public class Brick  {
         return out;
     }
 
-    public final boolean isBroken(){
-        return broken;
+    public void impact(){
+        strength--;
+        broken = (strength == 0);
     }
 
     public void repair() {
         broken = false;
         strength = fullStrength;
     }
-
-    public void impact(){
-        strength--;
-        broken = (strength == 0);
+    
+    public Color getBorderColor(){
+        return  border;
     }
 
-
+    public Color getInnerColor(){
+        return inner;
+    }
 
 }
 
