5c2954ce73552cbd8887a4f7c5a829f94e2df1ea
Carolyn777777712 <Carolyn777777712@gmail.com>
2021-12-10 21:31:51 +0800

    Add javadoc comments for ScoreSorting

--- src/main/java/com/hfych2/brickdestroy/model/ScoreSorting.java
+++ src/main/java/com/hfych2/brickdestroy/model/ScoreSorting.java
@@ -2,18 +2,30 @@ package com.hfych2.brickdestroy.model;
 
 import java.util.Comparator;
 
+/**
+ * This class is used for score sorting functionality on sorting the highScoresList.txt
+ */
 public class ScoreSorting {
 
     private String name;
     private int thisScore;
     private int thisLevel;
 
+    /**
+     * Class constructor.
+     * @param name      the name.
+     * @param thisScore the score
+     * @param thisLevel the level
+     */
     public ScoreSorting (String name, int thisScore, int thisLevel) {
         this.name = name;
         this.thisScore = thisScore;
         this.thisLevel = thisLevel;
     }
 
+    /**
+     * Custom comparator to compare and sort the scores of the users in ascending order.
+     */
     public static Comparator<ScoreSorting> sortScore = (p1, p2) -> {
 
         int score1 = p1.getThisScore();
@@ -23,11 +35,20 @@ public class ScoreSorting {
 
 
     };
+
+    /**
+     * String representation of the ArrayList elements.
+     * @return String representation of the ArrayList elements.
+     */
     @Override
     public String toString() {
         return "[Name: " + name + "\tScore: " + thisScore + "\tLevel: " + thisLevel + "]\n";
     }
 
+    /**
+     * Gets the score.
+     * @return the score.
+     */
     public int getThisScore() {
         return thisScore;
     }
