96b532e280465b2f6f339f221b594e3a3ee42449
Carolyn777777712 <Carolyn777777712@gmail.com>
2021-12-10 23:02:37 +0800

    Add javadoc comments for GameView and rename variables where appropriate

--- src/main/java/com/hfych2/brickdestroy/view/GameView.java
+++ src/main/java/com/hfych2/brickdestroy/view/GameView.java
@@ -11,7 +11,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.font.FontRenderContext;
 
-
+/**
+ * This class is extracted from {@link GameBoard} of the original code
+ *      and has been modified along with having additions to become a view class for
+ *      the model {@link GameBoard}
+ *      and the controller {@link com.hfych2.brickdestroy.controller.GameController}
+ */
 public class GameView extends JComponent {
 
     private GameBoard gameBoard;
@@ -36,11 +41,17 @@ public class GameView extends JComponent {
 
     private String scoreMessage;
 
-
+    /**
+     * Class constructor.<br>
+     * Calls the {@link GameView#initialise()}.<br>
+     */
     public GameView(){
         initialise();
     }
 
+    /**
+     * Initialises the view.
+     */
     private void initialise()
     {
         menuFont = new Font("Monospaced",Font.PLAIN,TEXT_SIZE);
@@ -51,10 +62,23 @@ public class GameView extends JComponent {
 
     }
 
+    /**
+     * Gets the gameBoard to update the states.
+     * @param gameBoard the gameBoard.
+     */
     public void updateView(GameBoard gameBoard){
         this.gameBoard = gameBoard;
     }
 
+    /**
+     * Paints the view and sets the {@link GameView#setScoreMessage(String)}.<br>
+     * Calls {@link GameView#drawBall(Ball, Graphics2D)},
+     *       {@link GameView#drawPlayer(Player, Graphics2D)}, and
+     *       {@link GameView#drawMenu(Graphics2D)},
+     *       {@link GameView#drawBrick(Brick, Graphics2D)}
+     *       depending on the state of the model {@link GameBoard}
+     * @param g
+     */
     public void paint(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
@@ -81,6 +105,10 @@ public class GameView extends JComponent {
         setScoreMessage("Time Taken (mm:ss)" +gameBoard.getFormatMinutes() +":" + gameBoard.getFormatSeconds());
     }
 
+    /**
+     * Clears the view.
+     * @param g2d the Graphics2D
+     */
     private void clear(Graphics2D g2d) {
         Color tmp = g2d.getColor();
         g2d.setColor(BG_COLOR);
@@ -88,6 +116,11 @@ public class GameView extends JComponent {
         g2d.setColor(tmp);
     }
 
+    /**
+     * Draws the brick to view.
+     * @param brick the brick
+     * @param g2d the Graphics2D
+     */
     private void drawBrick(Brick brick, Graphics2D g2d) {
         Color tmp = g2d.getColor();
 
@@ -101,6 +134,11 @@ public class GameView extends JComponent {
         g2d.setColor(tmp);
     }
 
+    /**
+     * Draws the ball to view.
+     * @param ball the ball.
+     * @param g2d the Graphics2D
+     */
     private void drawBall(Ball ball, Graphics2D g2d) {
         Color tmp = g2d.getColor();
 
@@ -115,10 +153,15 @@ public class GameView extends JComponent {
         g2d.setColor(tmp);
     }
 
-    private void drawPlayer(Player p, Graphics2D g2d) {
+    /**
+     * Draws the player to view.
+     * @param player the player.
+     * @param g2d the Graphics2D.
+     */
+    private void drawPlayer(Player player, Graphics2D g2d) {
         Color tmp = g2d.getColor();
 
-        Shape s = p.getPlayerFace();
+        Shape s = player.getPlayerFace();
         g2d.setColor(Player.INNER_COLOR);
         g2d.fill(s);
 
@@ -128,11 +171,21 @@ public class GameView extends JComponent {
         g2d.setColor(tmp);
     }
 
+    /**
+     * Calls {@link GameView#obscureGameBoard(Graphics2D)}
+     *       and {@link GameView#drawPauseMenu(Graphics2D)}
+     *       to draw the PauseMenu to view.
+     * @param g2d the Graphics2D
+     */
     private void drawMenu(Graphics2D g2d) {
         obscureGameBoard(g2d);
         drawPauseMenu(g2d);
     }
 
+    /**
+     * Draws the background of the PauseMenu.
+     * @param g2d the Graphics2D.
+     */
     private void obscureGameBoard(Graphics2D g2d) {
 
         Composite tmp = g2d.getComposite();
@@ -148,6 +201,10 @@ public class GameView extends JComponent {
         g2d.setColor(tmpColor);
     }
 
+    /**
+     * Draws the PauseMenu to view.
+     * @param g2d the Graphics2D.
+     */
     private void drawPauseMenu(Graphics2D g2d) {
         Font tmpFont = g2d.getFont();
         Color tmpColor = g2d.getColor();
@@ -201,23 +258,42 @@ public class GameView extends JComponent {
         g2d.setColor(tmpColor);
     }
 
+    /**
+     * Sets the message to be displayed.
+     * @param message the message.
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Sets the scoreMessgae to be displayed.
+     * @param scoreMessage the scoreMessage.
+     */
     public void setScoreMessage(String scoreMessage) {
         this.scoreMessage = scoreMessage;
     }
 
-
+    /**
+     * Gets the rectangle continueButton.
+     * @return the rectangle continueButton.
+     */
     public Rectangle getContinueButtonRect() {
         return continueButtonRect;
     }
 
+    /**
+     * Gets the rectangle exitButton.
+     * @return the rectangle exitButton.
+     */
     public Rectangle getExitButtonRect() {
         return exitButtonRect;
     }
 
+    /**
+     * Gets the rectangle restartButton.
+     * @return the rectangle restartButton.
+     */
     public Rectangle getRestartButtonRect() {
         return restartButtonRect;
     }
